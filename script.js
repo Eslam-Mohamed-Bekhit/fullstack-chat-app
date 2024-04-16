ee096379299dc3b0751a1e7c3adefa77d65281e4
Eslam-Mohamed-Bekhit <islam.m.bekhit@gmail.com>
2024-04-17 01:50:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+let localStream;
+let remoteStream;
+let localVideo = document.getElementById('localVideo');
+let remoteVideo = document.getElementById('remoteVideo');
+
+
+startButton.addEventListener('click', startChat);
+stopButton.addEventListener('click', stopChat);
+
+let socket = io();
+
+async function startChat() {
+    try {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localVideo.srcObject = localStream;
+
+        socket.emit('joinRoom'); // انضم إلى غرفة الدردشة
+
+        socket.on('remoteStream', function (stream) {
+            remoteStream = stream;
+            remoteVideo.srcObject = remoteStream;
+        });
+
+    } catch (error) {
+        console.error('Error starting chat:', error);
+    }
+}
+
+function stopChat() {
+    socket.emit('leaveRoom'); // انسحاب من الغرفة
+    if (localStream) {
+        localStream.getTracks().forEach(track => track.stop());
+    }
+    if (remoteStream) {
+        remoteStream.getTracks().forEach(track => track.stop());
+    }
+}
