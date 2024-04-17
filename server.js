5be577a3d3d1ccc39e9bc19832cda1ec416ad8f5
Eslam-Mohamed-Bekhit <islam.m.bekhit@gmail.com>
2024-04-17 18:41:22 +0200

    second

--- server.js
+++ server.js
@@ -11,9 +11,6 @@ const PORT = process.env.PORT || 3000;
 
 // تقديم الملفات الثابتة
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', (req, res) => { res.send("<p>some html</p>") });
-app.use('/api', (req, res) => { res.send("<p>some html from api</p>") });
 io.on('connection', (socket) => {
     console.log('A user connected');
 
